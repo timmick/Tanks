96f763847a97e1138902d72c2f0be9917737edc1
Atabaev Temirhan <atabaev_t@auca.space>
2019-02-21 13:56:25 +0000

    closes #3,closes #4 Реализовать код ввода с клавиатуры для управления танком Добавить код передачи команд на сервер

--- tanks.js
+++ tanks.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 8004
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
 app.use(express.static('public'))
 
-app.listen(port,() =>
+io.on('connection', socket => 
+{
+  socket.on('cmd', key => console.log(key))
+})
+
+http.listen(port,() =>
 {
 	console.log(`The Tanks server is listening on port ${port}`)
 }) 
